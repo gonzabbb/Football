08cca0e8711af94ce4effa97d3f8900630fc0080
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-08-31 19:05:49 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Abraham Gonzalez on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
